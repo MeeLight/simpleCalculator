cadf445728f708ed2991857d0218054c235e8749
MeeLight <moisesreyes0112@gmail.com>
2024-05-28 05:20:11 -0400

    removing unnecessary comment XD

--- commands/build.bat
+++ commands/build.bat
@@ -1,6 +1,5 @@
 @echo off
 
-:: cd ./.. & if not exist "build" then md "build" & cd ./build & kotlinc ./../src/main/kotlin/Main.kt -include-runtime -d simpleCalculator.jar & cd ./..
 cd ./..
 
 if not exist "build" (
